2736fab042411fa1aecfcf99c361366c67c005b5
Alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2023-04-25 21:42:39 +0400

    first commit

---
+++ Generic Type, Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Generic_Type__Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> collection = new SortedList<int, string>();
+            collection.Add(1995, "PHP");
+            collection.Add(1974, "SQL");
+            collection.Add(1983, "C++");
+            collection.Add(1957, "Lisp");
+            collection.Add(1996, "Ruby");
+            collection.Add(2009, "Go");
+            collection.Add(1950, "Assembly");
+            collection.Add(1959, "Cobol");
+            collection.Add(2010, "Rust");
+            collection.Add(1980, "Ada");
+            collection.Add(1991, "Python");
+            collection.Add(1972, "Pascal");
+            collection.Add(2014, "Swift"); 
+
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item.Key +" "+ item.Value);
+
+            }
+        }
+    }
+}
